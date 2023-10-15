f8714790a78befd5adad6f187cf9a3fae13be138
Atkinss1 <jordanatkins@live.com>
2023-10-15 12:19:34 -0600

    adjusted center for exit text

--- welcome-alarm.js
+++ welcome-alarm.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const welcomeFunc = () => {
   process.stdout.write(`\n\n\t\t\t 🕐🕐 ${chalk.underline.bold.green('Welcome to the interactive alarm clock!')}🕐🕐
   \t\t  press 'b' to set the alarm off, or choose 1 - 9 to set a timer
-\n\t\t\t\t       ${chalk.yellow('CTRL + C to exit\n\n')}`);
+\n\t\t\t\t        ${chalk.yellow('CTRL + C to exit\n\n')}`);
 };
 
 module.exports = welcomeFunc;
