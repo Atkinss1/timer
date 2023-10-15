f17e01d108ebd66f31162aa9dbe5a4ec716682b0
Atkinss1 <jordanatkins@live.com>
2023-10-15 12:11:27 -0600

    add chalk package and emjois to text

--- welcome-alarm.js
+++ welcome-alarm.js
@@ -1,8 +1,9 @@
+const chalk = require('chalk');
+
 const welcomeFunc = () => {
-  process.stdout.write(`\n\n\t\t\t   Welcome to the interactive alarm clock!
-  \t\tpress 'b' to set the alarm off, or choose 1 - 9 to set a timer
-\n\t\t\t\t   use CTRL + C to exit\n\n`);
+  process.stdout.write(`\n\n\t\t\t 🕐🕐 ${chalk.underline.bold.green('Welcome to the interactive alarm clock!')}🕐🕐
+  \t\t  press 'b' to set the alarm off, or choose 1 - 9 to set a timer
+\n\t\t\t\t       ${chalk.yellow('CTRL + C to exit\n\n')}`);
 };
 
-welcomeFunc();
 module.exports = welcomeFunc;
